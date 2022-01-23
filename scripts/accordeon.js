58798a144d341e9c992d2db8e11acf2d7c781d6e
Nikita <ncheusov@inbox.ru>
2022-01-23 21:22:45 +0300

    Сделал дополнительно, чтобы все табы при загрузке страницы были закрыты и при открытии одного контентного блока остальные закрывались

--- scripts/accordeon.js
+++ scripts/accordeon.js
@@ -1,22 +1,30 @@
 const accordeon = () => {
     const chItems = document.querySelectorAll('.characteristics__item');
 
-    chItems.forEach(item => {
+    chItems.forEach((item, index) => {
         const chButton = item.querySelector('.characteristics__title');
-        const chContent = item.querySelector('.characteristics__description');
         
-        chButton.addEventListener('click', () => {
+    const closeAccordeonItem = (item) => {
+        const chButton = item.querySelector('.characteristics__title'),
+                chContent = item.querySelector('.characteristics__description');
+
             if (chContent.classList.contains('open')) {
-
                 chContent.style.height = '';
-
             } else {
-
                 chContent.style.height = chContent.scrollHeight + 'px';
-
             }
             chButton.classList.toggle('active');
             chContent.classList.toggle('open');
+        };
+
+        chButton.addEventListener('click', () => {
+            chItems.forEach((closeItem, closeIndex) => {
+                if (closeItem.querySelector('.characteristics__description').classList.contains('open') && index != closeIndex) {
+                    closeAccordeonItem(closeItem);
+                }
+            });
+
+            closeAccordeonItem(item);
         });
     });
 };
